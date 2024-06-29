7d284578be9a1bc0374575758ebdf945bab57312
unknown <vneki021@gmail.com>
2024-06-29 23:54:08 +0200

    [feature] service repository for playlists

---
+++ App/Back/Domain/PlayLista.cs
@@ -0,0 +1,42 @@
+﻿namespace App.Back.Domain
+{
+    public class PlayLista
+    {
+        public int Id { get; set; }
+        public int IdKorisnika { get; set; }
+        public List<int> Pesme { get; set; }
+
+        public PlayLista()
+        {
+            Pesme = new List<int>();
+        }
+        public PlayLista(int id, int idKorisnika, List<int> pesme)
+        {
+            Id = id;
+            IdKorisnika = idKorisnika;
+            Pesme = pesme;
+        }
+        public PlayLista(int id, Nalog korisnik, List<MuzickoDelo> pesme)
+        {
+            Id = id;
+            IdKorisnika = korisnik.Id;
+            Pesme = new List<int>();
+            FillPesme(pesme);
+
+        }
+        public PlayLista(PlayLista other)
+        {
+            Id = other.Id;  
+            IdKorisnika = other.IdKorisnika;
+            Pesme = new(other.Pesme);
+        }
+        private void FillPesme(List<MuzickoDelo> pesme)
+        {
+            foreach (MuzickoDelo md in pesme)
+            {
+                if (Pesme.Contains(md.Id))
+                    Pesme.Add(md.Id);
+            }
+        }
+    }
+}
---
+++ App/Back/Repository/TipUcesnikaUcesceConnectorRepository.cs
@@ -0,0 +1,73 @@
+﻿using App.Back.Domain;
+using App.Back.Domain.Connectors;
+using App.Back.Repository.Base;
+using App.Back.Repository.Interface;
+using App.Back.Utilities;
+
+namespace App.Back.Repository
+{
+    public class TipUcesnikaUcesceConnectorRepository : Repository<TipUcesnikaUcesceConnector>, IRepository<TipUcesnikaUcesceConnector>
+    {
+        public TipUcesnikaUcesceConnectorRepository()
+        {
+            SetFileName("TipUcesnikaUcesceConnectors.json");
+        }
+        public TipUcesnikaUcesceConnector? Create(TipUcesnikaUcesceConnector instance)
+        {
+            var fetchedInstance = Get(instance);
+
+            if (fetchedInstance != null) { return null; }
+
+            var instances = GetAll();
+            instances.Add(instance);
+            Save(instances);
+
+            return instance;
+        }
+
+        public TipUcesnikaUcesceConnector? Delete(TipUcesnikaUcesceConnector instance)
+        {
+            var fetchedInstance = Get(instance);
+
+            if (fetchedInstance == null) { return null; }
+
+            var instances = Load();
+            instances.Remove(instance);
+            Save(instances);
+
+            return instance;
+        }
+
+        public TipUcesnikaUcesceConnector? Get(TipUcesnikaUcesceConnector instance)
+        {
+            foreach (var izvodjac in GetAll())
+            {
+                if (izvodjac.IdTipUcesnika == instance.IdTipUcesnika &&
+                    izvodjac.IdUcesce == instance.IdUcesce)
+                {
+                    return izvodjac;
+                }
+            }
+            return null;
+        }
+
+        public List<TipUcesnikaUcesceConnector> GetAll()
+        {
+            return Load();
+        }
+
+        public TipUcesnikaUcesceConnector? Update(TipUcesnikaUcesceConnector instance)
+        {
+            var fetchedInstance = Get(instance);
+
+            if (fetchedInstance == null) { return null; }
+
+            var instances = Load();
+            instances.Remove(fetchedInstance);
+            instances.Add(instance);
+            Save(instances);
+
+            return instance;
+        }
+    }
+}
