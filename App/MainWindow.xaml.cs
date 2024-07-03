cb6b69dbe1ab06397c0908560c106968f0a4aa1c
Stefan Stjepic <stefan.stjepic03@gmail.com>
2024-07-03 14:09:51 +0200

    [UPDATE]Main window

--- App/MainWindow.xaml.cs
+++ App/MainWindow.xaml.cs
@@ -12,28 +12,11 @@ namespace App
         {
             InitializeComponent();
         }
-
-        private void KreiraIzvodjacaClick(object sender, RoutedEventArgs e)
-        {
-            var window = new PerformerCreation();
-            window.ShowDialog();
-        }
-        private void KreiranjeDelaClick(object sender, RoutedEventArgs e)
-        {
-            MusicalPieceView musicalPieceView  = new MusicalPieceView(null);
-            musicalPieceView.Show();
-        }
         private void LoginClick(object sender, RoutedEventArgs e)
         {
             var window = new Login();
             var a = window.ShowDialog();
             if (a != null && a == true) { Close(); }
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            CreateMusicEditorView createMusicEditorView = new CreateMusicEditorView(null);
-            createMusicEditorView.Show();
         }
     }
 }
